a29d5a3edebc3c9f703d609eaf73c2a1087c55fc
Anthony Metzidis <anthony.metzidis@gmail.com>
2017-01-26 10:50:31 -0800

    add passport and dockerfile

--- config.js
+++ config.js
@@ -33,13 +33,9 @@
   },
 
   "storage": {
-    "type": "redis",
-    "host": "0.0.0.0",
-    "port": 6379,
-    "db": 2,
-    "expire": 2592000
+    "type": "file",
+    "path": "./data"
   },
-
   "documents": {
     "about": "./about.md"
   }
