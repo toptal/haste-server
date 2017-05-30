9e7e999b9d8ef45cdae11989df6339337cd25476
Amer Almadani <amer@ma.tc>
2017-05-31 00:34:13 +0300

    changed to file type
    Heroku testing

--- config.js
+++ config.js
@@ -33,11 +33,8 @@
   },
 
   "storage": {
-    "type": "redis",
-    "host": "0.0.0.0",
-    "port": 6379,
-    "db": 2,
-    "expire": 2592000
+  "path": "./data",
+  "type": "file"
   },
 
   "documents": {
