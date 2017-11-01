40f1f2588ed06f599ac1d29ad166f25bac10bb6a
John Crepezzi <john.crepezzi@gmail.com>
2017-10-31 20:55:51 -0400

    Update some es6

--- test/key_generators/random_spec.js
+++ test/key_generators/random_spec.js
@@ -4,15 +4,15 @@ const assert = require('assert');
 
 const Generator = require('../../lib/key_generators/random');
 
-describe('RandomKeyGenerator', function() {
-  describe('randomKey', function() {
-    it('should return a key of the proper length', function() {
-      var gen = new Generator();
+describe('RandomKeyGenerator', () => {
+  describe('randomKey', () => {
+    it('should return a key of the proper length', () => {
+      const gen = new Generator();
       assert.equal(6, gen.createKey(6).length);
     });
 
     it('should use a key from the given keyset if given', () => {
-      var gen = new Generator({keyspace: 'A'});
+      const gen = new Generator({keyspace: 'A'});
       assert.equal('AAAAAA', gen.createKey(6));
     });
   });
