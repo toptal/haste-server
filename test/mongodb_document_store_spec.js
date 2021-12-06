5353c1757b81b4da5b023a2be510d29201f999a6
Bharath S <36472845+bharathks005@users.noreply.github.com>
2021-12-06 11:19:44 +0530

    Update mongodb_document_store_spec.js
    removed unwanted codes

--- test/mongodb_document_store_spec.js
+++ test/mongodb_document_store_spec.js
@@ -15,27 +15,4 @@ describe('mongodb_document_store', () => {
             });
         })
     });
-
-    // it('should create new task', () => {
-    //     return service.addTask({ name: 'next', completed: false })
-    //         .then(task => {
-    //             expect(task.name).to.equal('next')
-    //             expect(task.completed).to.equal(false)
-    //         })
-    //         .then(() => service.getTasks())
-    //         .then(tasks => {
-    //             expect(tasks.length).to.equal(2)
-    //             expect(tasks[1].name).to.equal('next')
-    //         })
-    // })
-
-    // it('should remove task', () => {
-    //     return service.getTasks()
-    //         .then(tasks => tasks[0]._id)
-    //         .then(taskId => service.deleteTask(taskId))
-    //         .then(() => service.getTasks())
-    //         .then(tasks => {
-    //             expect(tasks.length).to.equal(0)
-    //         })
-    // })
 })
